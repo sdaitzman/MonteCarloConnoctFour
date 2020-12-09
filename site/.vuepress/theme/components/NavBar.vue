e79553392965b1e4f768d5afb4ae9390f2c6132c
Sam Daitzman <sam@daitzman.com>
2020-12-09 13:55:08 -0500

    sets up to style sidebar

--- site/.vuepress/theme/components/NavBar.vue
+++ site/.vuepress/theme/components/NavBar.vue
@@ -3,10 +3,10 @@
     <ul>
       <li><router-link to="/connect-four/">Connect Four</router-link></li>
       <li><router-link to="/mcts/">MCTS</router-link></li>
-      <li><router-link to="/heuristics/">heuristics</router-link></li>
-      <li><router-link to="/performance/">performance</router-link></li>
-      <li><router-link to="/discussion/">discussion</router-link></li>
-      <li><router-link to="/bibliography/">bibliography</router-link></li>
+      <li><router-link to="/heuristics/">Heuristics</router-link></li>
+      <li><router-link to="/performance/">Performance</router-link></li>
+      <li><router-link to="/discussion/">Discussion</router-link></li>
+      <li><router-link to="/bibliography/">Bibliography</router-link></li>
     </ul>
   </div>
 </template>
