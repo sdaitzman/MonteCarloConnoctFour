aabbcb9d9e79c0f6ef91e2c1f735f39af7272fa4
Sam Daitzman <sam@daitzman.com>
2020-12-13 17:23:26 -0500

    functional hero button TODO: make it not ugly

---
+++ site/.vuepress/components/HeroButton.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <router-link :to="to">
+        <button class="hero-button">
+            <slot></slot>
+        </button>
+    </router-link>
+</div>
+</template>
+
+<script>
+/**
+  * A hero button for placing in text
+  */
+// import moment from "moment"
+export default {
+		name: "HeroButton",
+		data() {
+            console.log(this)
+           return {};
+		},
+		props: ['to']
+};
+
+</script>
+
+<style scoped>
+
+button::after { content: ' →' }
+
+.hero-button {
+    padding: 20px;
+    margin-bottom: 35px;
+    border: 3px solid black;
+}
+</style>
