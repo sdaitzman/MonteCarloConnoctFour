6ad7a6679f467358dc99d65c3bdd34fb5e792255
Sam Daitzman <sam@daitzman.com>
2020-12-14 08:13:46 -0500

    moves touchstart handler for iOS to correct vue handlers

--- site/.vuepress/theme/layouts/Layout.vue
+++ site/.vuepress/theme/layouts/Layout.vue
@@ -28,8 +28,13 @@ import 'normalize.css'
 export default {
   components: {
     NavBar
+  },
+  mounted() {
+    document.addEventListener("touchstart", ()=>{}, true)
+  },
+  updated() {
+    document.addEventListener("touchstart", ()=>{}, true)
   }
 }
 
-document.addEventListener("touchstart", ()=>{}, true)
 </script>
