4e4ceb53c77628e5fbb26a347acb910718db23a1
Sam Daitzman <sam@daitzman.com>
2020-12-14 13:57:04 -0500

    current page sidebar color indicator

--- site/.vuepress/theme/components/NavBar.vue
+++ site/.vuepress/theme/components/NavBar.vue
@@ -2,14 +2,14 @@
   <div class="nav-bar">
     <ul>
       <router-link to="/"><li class="logo-holder"><img src="logo.png" class="logo" alt="game tree of connect four" /></li></router-link>
-      <router-link to="/"><li><i class="fas fa-home"></i>Home</li></router-link>
-      <router-link to="/demo"><li><i class="fas fa-laptop-code"></i>Demo</li></router-link>
-      <router-link to="/team/"><li><i class="fas fa-users"></i>Team</li></router-link>
-      <router-link to="/connect-four/"><li><i class="fas fa-chess-board"></i>Connect Four</li></router-link>
-      <router-link to="/mcts/"><li><i class="fas fa-tree"></i>Monte Carlo Tree Search</li></router-link>
-      <router-link to="/minimax/"><li><i class="fas fa-chart-area"></i>Minimax</li></router-link>
-      <router-link to="/performance/"><li><i class="fas fa-bolt"></i>Performance</li></router-link>
-      <router-link to="/bibliography/"><li><i class="fas fa-book"></i>Bibliography</li></router-link>
+      <router-link class="home-link" to="/"><li><i class="fas fa-home"></i>Home</li></router-link>
+      <router-link class="demo-link" to="/demo"><li><i class="fas fa-laptop-code"></i>Demo</li></router-link>
+      <router-link class="team-link" to="/team/"><li><i class="fas fa-users"></i>Team</li></router-link>
+      <router-link class="conn-link" to="/connect-four/"><li><i class="fas fa-chess-board"></i>Connect Four</li></router-link>
+      <router-link class="mcts-link" to="/mcts/"><li><i class="fas fa-tree"></i>Monte Carlo Tree Search</li></router-link>
+      <router-link class="mnmx-link" to="/minimax/"><li><i class="fas fa-chart-area"></i>Minimax</li></router-link>
+      <router-link class="perf-link" to="/performance/"><li><i class="fas fa-bolt"></i>Performance</li></router-link>
+      <router-link class="bibl-link" to="/bibliography/"><li><i class="fas fa-book"></i>Bibliography</li></router-link>
     </ul>
   </div>
 </template>
